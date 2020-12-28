a8d81a8b9879d5c421ee7be2f9044404f90f31c5
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-12-28 19:49:19 +0100

    Enhance encapsulation

--- flappy/physics/motion.py
+++ flappy/physics/motion.py
@@ -5,20 +5,23 @@ from flappy.scene.board import Board
 
 class MotionEngine:
     def __init__(self, gravity=1.1):
-        self.last_update = time()
-        self.gravity = gravity
+        self.__last_update = time()
+        self.__gravity = gravity
 
     def update(self, board: Board):
         now = time()
-        delta = now - self.last_update
+        delta = now - self.__last_update
         self.move_terrain(board, delta)
         self.move_bird(board.bird, delta)
-        self.last_update = now
+        self.__last_update = now
+
+    def reset(self):
+        self.__last_update = time()
 
     def move_bird(self, bird: Bird, delta: float):
-        distance = bird.vertical_velocity * delta - (self.gravity * delta ** 2)/2
+        distance = bird.vertical_velocity * delta - (self.__gravity * delta ** 2)/2
         bird.frame.origin.y -= distance
-        bird.vertical_velocity = bird.vertical_velocity - self.gravity * delta
+        bird.vertical_velocity = bird.vertical_velocity - self.__gravity * delta
         bird.distance_travelled += bird.horizontal_velocity * delta
 
     @staticmethod
