c10b5ecc262adf6c60b215252d09bca6c82ef259
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-12-28 21:35:40 +0100

    Find position of the cursor within animation in a separate method

--- flappy/animation/sequence.py
+++ flappy/animation/sequence.py
@@ -1,6 +1,5 @@
-from collections import namedtuple
 from math import floor
-
+from collections import namedtuple
 from flappy.animation.stopwatch import StopWatch
 
 Keyframe = namedtuple('Keyframe', ['image', 'length'])
@@ -27,12 +26,16 @@ class Animation:
     def pause(self):
         self.timer.pause()
 
-    def image(self):
+    def cursor(self):
         elapsed = self.timer.elapsed_time()
         if self.repeat:
             animation_length = self.animation_length()
             cycles = floor(elapsed / animation_length)
             elapsed = elapsed - cycles * animation_length
+        return elapsed
+
+    def image(self):
+        elapsed = self.cursor()
         for frame in self.keyframes:
             begin = 0
             if begin <= elapsed < begin + frame.length:
