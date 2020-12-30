c1a6e5d6b12fba999866eb98b64208e5c4716653
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-12-30 13:54:47 +0100

    Implement stack

---
+++ flappy/core/stack.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+
+class Stack:
+    def __init__(self):
+        self.__items = []
+
+    def pop(self) -> Any:
+        return self.__items.pop(len(self.__items)-1)
+
+    def push(self, item: Any):
+        self.__items.append(item)
+
+    def clear(self):
+        self.__items = []
+
+    def top(self):
+        return self.__items[len(self.__items) - 1]
+
+    def __bool__(self):
+        return bool(self.__items)
+
+    def __len__(self):
+        return len(self.__items)
