53619c50d161959921fdccde3357c12cd6f308d2
Mateusz Stompór <mateusz.stompor@ibm.com>
2021-01-02 01:08:19 +0100

    Add methods for converting from and to pygame

--- flappy/visualizer/conversion/rect.py
+++ flappy/visualizer/conversion/rect.py
@@ -17,3 +17,11 @@ class RectConverter:
     def from_pygame(rect: PRect, screen: Surface) -> Rect:
         return Rect(Point(rect[0] / screen.get_width(), rect[1] / screen.get_height()),
                     Size(rect[2] / screen.get_width(), rect[3] / screen.get_height()))
+
+    @staticmethod
+    def convert_to_pygame(rect: Rect):
+        return PRect(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
+
+    @staticmethod
+    def convert_from_pygame(rect: PRect):
+        return Rect(Point(rect[0], rect[1]), Size(rect[2], rect[3]))
