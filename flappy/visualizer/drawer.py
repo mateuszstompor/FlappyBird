95d0d99b83b7ac6fc934fad2ac8c043a22f40c93
Mateusz Stompór <mateusz.stompor@ibm.com>
2021-01-02 02:25:08 +0100

    Generate abilities to generate red pipes

--- flappy/visualizer/drawer.py
+++ flappy/visualizer/drawer.py
@@ -8,13 +8,13 @@ from flappy.gmath.point import Point
 from flappy.scene.board import Board
 from flappy.scene.obstacle import Obstacle
 from flappy.textures.library import TextureLibrary
-from flappy.visualizer.common import render_centered
 from flappy.visualizer.conversion.rect import RectConverter
 
 
 class Drawer:
     def __init__(self):
         self.textures = TextureLibrary.with_images(['pipe-green.png',
+                                                    'pipe-red.png',
                                                     'redbird-downflap.png',
                                                     'background-day.png',
                                                     'background-night.png',
@@ -22,10 +22,10 @@ class Drawer:
 
     def draw(self, surface: Surface, board: Board):
         self.present_bird(surface, board.bird)
-        for obstacle in board.obstacles:
+        for index, obstacle in enumerate(board.obstacles):
             if Drawer.is_in_sight(obstacle, Rect(Point(-1, 0), Size(3, 1))):
-                self.present_wall(surface, obstacle)
-        render_centered(self.textures['base.png'], surface, Point(0, 260))
+                image_name = 'pipe-red.png' if index % 5 == 0 and index > 0 else 'pipe-green.png'
+                self.present_wall(surface, obstacle, image_name)
 
     @staticmethod
     def present_bird(surface: Surface, bird: Bird):
@@ -33,10 +33,10 @@ class Drawer:
         image = pygame.transform.rotate(bird.animation.data(), bird.state.angle)
         surface.blit(image, rect)
 
-    def present_wall(self, surface: Surface, obstacle: Obstacle):
+    def present_wall(self, surface: Surface, obstacle: Obstacle, image_name: str):
         upper, lower = obstacle.wall
         rect = RectConverter.as_pygame(lower, surface)
-        image = self.textures.image('pipe-green.png')
+        image = self.textures[image_name]
         scale = (rect[2], image.get_rect()[3])
         scaled = pygame.transform.scale(image, scale)
         surface.blit(scaled, (rect[0], rect[1], scale[0], image.get_rect()[3]))
