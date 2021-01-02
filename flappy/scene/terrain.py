458b9a1d902a96b45bea16d67f402f5ff118d9e1
Mateusz Stompór <mateusz.stompor@ibm.com>
2021-01-02 02:25:32 +0100

    Fix pylint R0903

--- flappy/scene/terrain.py
+++ flappy/scene/terrain.py
@@ -1,19 +1,23 @@
-from random import uniform
-from random import choice
+from random import uniform, choice
+
+from flappy.gmath.rect import Rect
 from flappy.gmath.frange import Frange
 from flappy.scene.obstacle import Obstacle
 
 
-class TerrainGenerator:  # pylint: disable=R0903
+class TerrainGenerator:
     def __init__(self, y_delta: Frange, x_delta: float, bound: Frange):
         self.y_delta = y_delta
         self.x_delta = x_delta
         self.bound = bound
 
-    def new(self, previous: Obstacle) -> Obstacle:
-        new = previous.gap
+    def generate_gap(self, previous: Rect) -> Rect:
+        new = previous
         new.origin.x = max(new.origin.x + self.x_delta, 1.1)
         y_displacement = uniform(self.y_delta.low, self.y_delta.high)
         new.origin.y += choice([-1, 1]) * y_displacement
         new.origin.y = min(max(new.origin.y, self.bound.low), self.bound.high)
-        return Obstacle(new)
+        return new
+
+    def new(self, previous: Obstacle) -> Obstacle:
+        return Obstacle(self.generate_gap(previous.gap))
