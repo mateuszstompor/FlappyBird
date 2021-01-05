118ba630e334093d74099d92de8c8ec90d0cd297
Mateusz Stompór <mateusz.stompor@ibm.com>
2021-01-05 18:05:13 +0100

    Remove unused code

--- flappy/visualizer/common.py
+++
@@ -1,9 +0,0 @@
-from pygame.surface import Surface
-from flappy.gmath.point import Point
-from flappy.extensions.rect import adding, center
-
-
-def render_centered(image: Surface, surface: Surface, offset: Point = Point(0, 0)):
-    centered = center(image.get_rect(), surface.get_rect(), x_axis=True, y_axis=True)
-    rect = adding(centered, offset)
-    surface.blit(image, rect)
